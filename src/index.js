7d90815161c825ee2a943a97c88cbde2a8949b5b
grnsmn <simoneguarnuccio@gmail.com>
2022-12-29 18:29:59 +0100

    fix: modify structure and fixed width styled on video

--- src/index.js
+++ src/index.js
@@ -2,17 +2,21 @@
 import React from 'react';
 
 const Video = (props) => {
+
     return (
         <div>
-            <video
+             <video
                 src={props.source.uri}
-                style={props.style}
-                loop={props.repeat}
+                style={{
+                    ...props.style,
+                    objectFit: props.resizeMode==='cover'?'cover':'contain' 
+                }}
+                loop={props.repeat}   
                 onError={props.onError}
                 onLoadedData={props.onLoad}
                 onLoadStart={props.onLoadStart}
                 autoPlay={!props.paused}
-                muted 
+                muted
             />
         </div>
     );
