1eae0c5a954361369fb30f9a23f63b05bc0bd427
grnsmn <simoneguarnuccio@gmail.com>
2022-12-29 14:56:33 +0100

    feat: add remapped attributed

--- Video.js
+++ Video.js
@@ -3,10 +3,14 @@ import React from 'react';
 const Video = props => {
     return (
         <div>
-            <video
+        <video
                 src={props.source.uri}
-                style={props.style}    
-                autoPlay
+                style={props.style}
+                loop={props.repeat}   
+                onError={props.onError}
+                onLoad={props.onLoad}
+                onLoadStart={props.onLoadStart}
+                autoPlay={!props.paused}
                 muted
             />
         </div>
