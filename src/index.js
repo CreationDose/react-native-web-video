9f6b6cc36e4d452c91c584d8f5ff5eec32de8a74
grnsmn <simoneguarnuccio@gmail.com>
2022-12-30 09:57:20 +0100

    fix: video proptypes added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Video = (props) => {
-
     return (
         <div>
-             <video
+            <video
                 src={props.source.uri}
                 style={{
                     ...props.style,
-                    objectFit: props.resizeMode==='cover'?'cover':'contain' 
+                    objectFit: props.resizeMode === 'cover' ? 'cover' : 'contain'
                 }}
-                loop={props.repeat}   
+                loop={props.repeat}
                 onError={props.onError}
                 onLoadedData={props.onLoad}
                 onLoadStart={props.onLoadStart}
@@ -21,5 +21,16 @@ const Video = (props) => {
         </div>
     );
 };
+
+Video.PropTypes = {
+    src: PropTypes.string.isRequired,
+    style: PropTypes.object,
+    loop: PropTypes.bool,
+    onError: PropTypes.func,
+    onLoadedData: PropTypes.func,
+    onLoadStart: PropTypes.func,
+    autoPlay: PropTypes.bool,
+    muted: PropTypes.bool
+}
 
 export default Video;
